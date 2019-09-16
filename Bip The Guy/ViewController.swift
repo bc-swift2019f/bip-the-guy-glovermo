3d688d7459bddd527ebc662bf997010e9cc33f22
Morgan Glover <glovermo@bc.edu>
2019-09-16 13:21:56 -0400

    4.1 - Completed; Basic UI

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func libraryPressed(_ sender: UIButton) {
+        print("Image Library pressed")
+    }
+    @IBAction func cameraPressed(_ sender: UIButton) {
+        print("Take Picture pressed")
+    }
+    @IBAction func imagePressed(_ sender: UITapGestureRecognizer) {
+        print("Image pressed")
+    }
+    
 }
 
