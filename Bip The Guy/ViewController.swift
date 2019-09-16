a4ccaa9da6dc97f3207ffdd6323113443611f21e
Morgan Glover <glovermo@bc.edu>
2019-09-16 13:50:27 -0400

    4.2 Completed - Added animation

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //MARK: Functions
+    func animateImage(){
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink imageToPunch by 60
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.minX + shrinkValue, y: self.imageToPunch.bounds.minY + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    //MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
         print("Image Library pressed")
     }
@@ -24,7 +37,7 @@ class ViewController: UIViewController {
         print("Take Picture pressed")
     }
     @IBAction func imagePressed(_ sender: UITapGestureRecognizer) {
-        print("Image pressed")
+        animateImage()
     }
     
 }
